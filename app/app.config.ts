fd75c9790955c3195dcc53997709b8572ce28c3c
Oliver Maglana <jamols09@gmail.com>
2026-02-26 19:13:00 +0800

    ~ use asset

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      dark: '/prof-pic.png',
+      light: '/img_prof3.png',
       alt: 'My profile picture'
     },
     meetingLink: 'https://cal.com/',
